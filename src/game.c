b13f86785e1accf0318e5e428feb7dab1c8ef039
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2020-04-03 20:26:52 +0200

    Fix incorrect undo on failed deal from stock.

--- src/game.c
+++ src/game.c
@@ -604,7 +604,9 @@ int turn_from_stock(Card *card, Pile *stock, Pile *piles) {
           turns++;
           card = get_top(stock->stack);
         } else {
-          undo_move();
+          if (turns > 0) {
+            undo_move();
+          }
           clear_redo_history();
           return 0;
         }
